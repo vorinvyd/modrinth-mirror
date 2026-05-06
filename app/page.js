a6bc28d0f666c9610640eb9fa1514145b6f7ccb5
БоБоБо <119269640+b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0@users.noreply.github.com>
2026-05-06 03:27:24 +0300

    - На главной странице привёл вывод версий Майнкрафт в автоматическом режиме. - Вывод количества всех проектов также выводится в автоматическом режиме. Ревалидейт... сутки.

--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
 import HomeClient from './HomeClient'
+import { getModrinthSixTypeCreationsTotal } from '@/lib/modrinthCatalogTotals'
 
 export const metadata = {
   title: 'ModrinthProxy',
   description: 'Скачать моды, плагины, шейдеры, ресурспаки и датапаки для Minecraft. Удобный каталог на русском языке. Тысячи модификаций для любой версии.',
 }
 
-export default function Home() {
-  return <HomeClient />
+export default async function Home() {
+  let catalogCreationsTotal = null
+  try {
+    const n = await getModrinthSixTypeCreationsTotal()
+    if (n > 0) catalogCreationsTotal = n
+  } catch {
+    catalogCreationsTotal = null
+  }
+  return <HomeClient catalogCreationsTotal={catalogCreationsTotal} />
 }
